895c86f3026e969e6cfa0d204a8612754568d1c4
Ogün EKİZ <ogunekiz@hotmail.com>
2025-03-09 23:36:03 +0300

    Add project files.

---
+++ TaskProject/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskProject.Models;
+
+namespace TaskProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ TaskProject/Program.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using TaskProject.Data;
+
+namespace TaskProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite("Data Source=Db/tasks.db"));
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
